b11ecd1fd37686bbf77f1213222de18abd182a59
Tuan Pham <phamtuanchip@gmail.com>
2019-08-24 16:42:11 +0700

    add spring gridfs

---
+++ spring-mongo-gridfs/src/main/java/com/javasampleapproach/gridfstemplate/SpringDataGridFsTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.gridfstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataGridFsTemplateApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataGridFsTemplateApplication.class, args);
+	}
+}
